d6e186ce82f297c5b564e3e77c4eb292cfe5f326
AburizalAN <56324469+AburizalAN@users.noreply.github.com>
2022-05-06 14:15:39 +0700

    Setup PWA

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
+
+const withPWA = require("next-pwa")
+
 const nextConfig = {
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+  },
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig)
---
+++ pages/_document.tsx
@@ -0,0 +1,19 @@
+import Document, { Html, Head, Main, NextScript, DocumentProps } from "next/document"
+
+function MyDocument(props: DocumentProps) {
+  return (
+    <Html>
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon.png"></link>
+        <meta name="theme-color" content="#fff" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
+
+export default MyDocument
